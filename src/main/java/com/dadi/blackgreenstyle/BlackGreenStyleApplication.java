df878661784e5a1b525a6c8dea4c19d98428525a
hardalec <alex_johnny@126.com>
2020-03-14 10:24:41 +0800

    first repo

---
+++ src/main/java/com/dadi/blackgreenstyle/BlackGreenStyleApplication.java
@@ -0,0 +1,13 @@
+package com.dadi.blackgreenstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackGreenStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackGreenStyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dadi/blackgreenstyle/BlackGreenStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadi.blackgreenstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackGreenStyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
